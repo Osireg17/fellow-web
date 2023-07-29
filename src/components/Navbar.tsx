d25eb6dfa73748d6e528dc7db9ee0f25616e1bd2
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2023-07-29 14:40:32 +0100

    Fixed Navbar open and closing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,17 @@ const Navbar = () => {
         setIsOpen(!isOpen)
     };
 
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
 
     return (
         <div className='bg-[#00000090] relative ease-in duration-300'>
             <div className='max-w-[1000px] m-auto flex justify-between items-center p-4 text-white'>
-                <Link href='/'>
+                <Link href='/' onClick={closeMenu}>
                     <Image src={Logos} alt='logo' width={100} height={100} />
                 </Link>
+                {/* other elements */}
                 <ul className='hidden sm:flex'>
                     <li className='p-4'>
                         <Link href='/'>Home</Link>
@@ -28,34 +32,29 @@ const Navbar = () => {
                             <button className='bg-white text-black mt-2 p-2 rounded-md hover:bg-gray-200'>Sign Up</button>
                         </Link>
                     </li>
-                    {/* <li className='p-4'>
-                    <Link href='/work'>App Design</Link>
-                </li> */}
                 </ul>
-
                 <div className='block sm:hidden z-30'>
                     {isOpen ? <VscClose onClick={toggle} className='text-4xl cursor-pointer z-20' /> : <VscMenu onClick={toggle} className='text-4xl cursor-pointer z-20' />}
                 </div>
-
                 <div className={
                     isOpen ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black z-20 text-center ease-in duration-300' :
                         'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black z-20 text-center ease-in duration-300'
                 }>
-                    <Link href='/' className='absolute top-0 left-0 right-0'>
+                    <Link href='/' onClick={closeMenu} className='absolute top-0 left-0 right-0'>
                         <Image src={Logo} alt='logo' width={100} height={100} />
                     </Link>
                     <ul className='relative z-10'>
                         <li className='p-4 text-4xl hover:text-gray-500'>
-                            <Link href='/'>Home</Link>
+                            <Link href='/' onClick={closeMenu}>
+                                <p>Home</p>
+                            </Link>
                         </li>
                         <li className='p-4 text-4xl hover:text-gray-500'>
-                            <Link href='/_signUp'>
+                            <Link href='/_signUp' onClick={closeMenu}>
                                 <button className='bg-white text-black p-2 rounded-md hover:bg-gray-200'>Sign Up</button>
                             </Link>
                         </li>
-                        {/* <li className='p-4 text-4xl hover:text-gray-500'>
-                        <Link href='/portfolio'>Work</Link>
-                    </li> */}
+                        {/* other elements */}
                     </ul>
                 </div>
             </div>
